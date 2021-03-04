12cf753e297c0250bef24d763bfc4177b3b89fd2
shiwuhao <shiwuhao@juzishuke.com>
2021-03-04 11:14:34 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.VUE_APP_API_URL,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': '/mock'
+    //     }
+    //   }
+    // }
+  },
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+};
