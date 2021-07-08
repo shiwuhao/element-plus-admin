e0f28a3ecf925cbf732e21e8176ae8948aed8bb8
shiwuhao <shiwuhao@juzishuke.com>
2021-07-08 18:30:46 +0800

    BasicForm

--- src/views/component/form/RuleForm.vue
+++ src/views/component/form/RuleForm.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-card class="m10" :header="cardTitle">
+  <h1>DDDD</h1>
+<!--  <el-card class="m10" :header="cardTitle">-->
 <!--    <BasicForm ref="formRef"-->
 <!--               v-model="form"-->
 <!--               :schemas="schemas"-->
@@ -9,47 +10,47 @@
 <!--               auto-width-->
 <!--               label-width="150px"-->
 <!--               label-position="right"></BasicForm>-->
-  </el-card>
+<!--  </el-card>-->
 </template>
 
 <script>
-import {getRuleFormData} from "@/views/component/form/formData";
-import {BasicForm} from "@/components/Form";
-
-export default {
-  name: 'RuleForm',
-  components: {BasicForm},
-  data() {
-    return {
-      cardTitle: this.$route.meta.title,
-      schemas: getRuleFormData(),
-
-      form: {
-        input: "",
-        input_number: 12321,
-        select: "value1",
-        time_picker: "16:40:33",
-        date_picker: "2021-06-18",
-        cascader: ["value2", "value1"],
-        slider: 17,
-        radio_group: "value2",
-        checkbox_group: ["value1", "value2"],
-        checkbox_button_group: ["value1"],
-        radio_button_group: "value2",
-        date_time_picker: "2021-06-17 00:00:00",
-        time_select: "11:00"
-      },
-    }
-  },
-  methods: {
-    handleReset() {
-      this.$refs['formRef'].resetFields();
-    },
-    handleSubmit() {
-      this.$refs['formRef'].validate((e) => {
-        console.log('e', e);
-      })
-    },
-  }
-}
+// import {getRuleFormData} from "@/views/component/form/formData";
+// import {BasicForm} from "@/components/Form";
+//
+// export default {
+//   name: 'RuleForm',
+//   components: {BasicForm},
+//   data() {
+//     return {
+//       cardTitle: this.$route.meta.title,
+//       schemas: getRuleFormData(),
+//
+//       form: {
+//         input: "",
+//         input_number: 12321,
+//         select: "value1",
+//         time_picker: "16:40:33",
+//         date_picker: "2021-06-18",
+//         cascader: ["value2", "value1"],
+//         slider: 17,
+//         radio_group: "value2",
+//         checkbox_group: ["value1", "value2"],
+//         checkbox_button_group: ["value1"],
+//         radio_button_group: "value2",
+//         date_time_picker: "2021-06-17 00:00:00",
+//         time_select: "11:00"
+//       },
+//     }
+//   },
+//   methods: {
+//     handleReset() {
+//       this.$refs['formRef'].resetFields();
+//     },
+//     handleSubmit() {
+//       this.$refs['formRef'].validate((e) => {
+//         console.log('e', e);
+//       })
+//     },
+//   }
+// }
 </script>
