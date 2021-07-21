fa9c83e1178fe7bb91adab057c813308188d85c2
shiwuhao <shiwuhao@juzishuke.com>
2021-07-21 18:39:10 +0800

    BasicForm

--- src/components/Tinymce/src/Tinymce.vue
+++ src/components/Tinymce/src/Tinymce.vue
@@ -1,175 +1,100 @@
 <template>
-  <textarea :id="tinymceId" ref="elRef" :style="{ visibility: 'hidden' }"></textarea>
+  <editor v-model="content" :init="options"/>
 </template>
 
 <script>
-import {RawEditorSettings} from 'tinymce';
+import {onMounted, ref} from 'vue';
 import tinymce from 'tinymce/tinymce';
-import 'tinymce/themes/silver';
-import 'tinymce/icons/default/icons';
-import 'tinymce/plugins/advlist';
-import 'tinymce/plugins/anchor';
-import 'tinymce/plugins/autolink';
-import 'tinymce/plugins/autosave';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/codesample';
-import 'tinymce/plugins/directionality';
-import 'tinymce/plugins/fullscreen';
-import 'tinymce/plugins/hr';
-import 'tinymce/plugins/insertdatetime';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/lists';
+import Editor from '@tinymce/tinymce-vue';
+import "tinymce/icons/default/icons";
+import 'tinymce/themes/silver/theme';
+import 'tinymce/plugins/image';
 import 'tinymce/plugins/media';
-import 'tinymce/plugins/nonbreaking';
-import 'tinymce/plugins/noneditable';
-import 'tinymce/plugins/pagebreak';
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/preview';
-import 'tinymce/plugins/print';
-import 'tinymce/plugins/save';
-import 'tinymce/plugins/searchreplace';
-import 'tinymce/plugins/spellchecker';
-import 'tinymce/plugins/tabfocus';
-// import 'tinymce/plugins/table';
-import 'tinymce/plugins/template';
-import 'tinymce/plugins/textpattern';
-import 'tinymce/plugins/visualblocks';
-import 'tinymce/plugins/visualchars';
-import 'tinymce/plugins/wordcount';
-import {ref, onMounted} from "vue";
-import plugins from "./plugins";
-import {buildShortUUID} from '@/utils/uuid';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/contextmenu';
+import 'tinymce/plugins/colorpicker';
+import 'tinymce/plugins/textcolor';
+import "tinymce/plugins/image";
+import "tinymce/plugins/media";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/contextmenu";
+import "tinymce/plugins/wordcount";
+import "tinymce/plugins/colorpicker";
+import "tinymce/plugins/textcolor";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/code";
+import "tinymce/plugins/link";
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/hr";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/textpattern";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/directionality";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/visualchars";
+import "tinymce/plugins/template";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/nonbreaking";
+import "tinymce/plugins/insertdatetime";
+import "tinymce/plugins/imagetools";
+import "tinymce/plugins/autosave";
+import "tinymce/plugins/autoresize";
+
 
 export default {
-  name: "Tinymce",
-  id: {
-    type: String,
-    default: function () {
-      return 'vue-tinymce-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
-    }
+  name: 'app',
+  components: {Editor},
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    plugins: {
+      type: [String, Array],
+      default:
+        "preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template code codesample table charmap hr nonbreaking insertdatetime advlist lists wordcount imagetools textpattern autosave autoresize"
+    },
+    toolbar: {
+      type: [String, Array],
+      default:
+        "code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link codesample | alignleft aligncenter alignright alignjustify outdent indent lineheight formatpainter | \
+    styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | \
+    table image media charmap hr pagebreak insertdatetime | bdmap fullscreen preview"
+    },
   },
-  options: {
-    type: Object,
-    default: () => ({}),
-  },
-  value: {
-    type: String,
-    default: ''
-  },
-  toolbar: {
-    type: Array,
-    required: false,
-    default() {
-      return []
-    }
-  },
-  menubar: {
-    type: String,
-    default: 'file edit insert view format table'
-  },
-  height: {
-    type: [Number, String],
-    required: false,
-    default: 360
-  },
-  width: {
-    type: [Number, String],
-    required: false,
-    default: 'auto'
-  },
-  setup(props) {
-    const tinymceId = ref(buildShortUUID('tinymce'));
-    let hasInit = false;
-    let fullscreen = false;
-    const languageTypeList = ref({
-      'en': 'en',
-      'zh': 'zh_CN',
-      'es': 'es_MX',
-      'ja': 'ja'
+  setup(props, {emit}) {
+    const content = ref();
+    const options = ref({
+      language_url: '/static/tinymce/langs/zh_CN.js',
+      language: 'zh_CN',
+      skin_url: '/static/tinymce/skins/ui/oxide',
+      height: 600,
+      plugins: props.plugins,
+      toolbar: props.toolbar,
+      toolbar_mode: 'sliding',
+      branding: true,
+      menubar: false,
+      //此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
+      //如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
+      images_upload_handler: (blobInfo, success, failure) => {
+        const img = 'data:image/jpeg;base64,' + blobInfo.base64()
+        success(img)
+      }
     });
-
-    function initTinymce() {
-      const _this = this
-      window.tinymce.init({
-        selector: `#${props.id}`,
-        language: languageTypeList.value['en'],
-        height: props.height,
-        body_class: 'panel-body ',
-        object_resizing: false,
-        toolbar: props.toolbar.length > 0 ? this.toolbar : toolbar,
-        menubar: props.menubar,
-        plugins: plugins,
-        end_container_on_empty_block: true,
-        powerpaste_word_import: 'clean',
-        code_dialog_height: 450,
-        code_dialog_width: 1000,
-        advlist_bullet_styles: 'square',
-        advlist_number_styles: 'default',
-        imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
-        default_link_target: '_blank',
-        link_title: false,
-        nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
-        init_instance_callback: editor => {
-          if (_this.value) {
-            editor.setContent(_this.value)
-          }
-          hasInit = true
-          editor.on('NodeChange Change KeyUp SetContent', () => {
-            this.hasChange = true
-            this.$emit('input', editor.getContent())
-          })
-        },
-        setup: (editor) => {
-          editor.on('FullscreenStateChanged', (e) => {
-            fullscreen = e.state
-          })
-        },
-        // it will try to keep these URLs intact
-        // https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@convert_urls/
-        // https://stackoverflow.com/questions/5196205/disable-tinymce-absolute-to-relative-url-conversions
-        convert_urls: false
-        // 整合七牛上传
-        // images_dataimg_filter(img) {
-        //   setTimeout(() => {
-        //     const $image = $(img);
-        //     $image.removeAttr('width');
-        //     $image.removeAttr('height');
-        //     if ($image[0].height && $image[0].width) {
-        //       $image.attr('data-wscntype', 'image');
-        //       $image.attr('data-wscnh', $image[0].height);
-        //       $image.attr('data-wscnw', $image[0].width);
-        //       $image.addClass('wscnph');
-        //     }
-        //   }, 0);
-        //   return img
-        // },
-        // images_upload_handler(blobInfo, success, failure, progress) {
-        //   progress(0);
-        //   const token = _this.$store.getters.token;
-        //   getToken(token).then(response => {
-        //     const url = response.data.qiniu_url;
-        //     const formData = new FormData();
-        //     formData.append('token', response.data.qiniu_token);
-        //     formData.append('key', response.data.qiniu_key);
-        //     formData.append('file', blobInfo.blob(), url);
-        //     upload(formData).then(() => {
-        //       success(url);
-        //       progress(100);
-        //     })
-        //   }).catch(err => {
-        //     failure('出现未知问题，刷新页面，或者联系程序员')
-        //     console.log(err);
-        //   });
-        // },
-      })
-    }
 
     onMounted(() => {
-      initTinymce();
-    });
+      tinymce.init(options.value)
+    })
 
     return {
-      tinymceId
+      content,
+      options
     }
   }
 }
