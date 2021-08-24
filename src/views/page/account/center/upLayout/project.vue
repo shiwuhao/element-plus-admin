50516a1d0de24a743dd7dba25aadd2f3520206de
wky0527 <805245271@qq.com>
2021-08-24 15:25:30 +0800

    feat: 完成个人中心页面上下布局的所有排版

---
+++ src/views/page/account/center/upLayout/project.vue
@@ -0,0 +1,31 @@
+<template>
+  <Card
+    :imageCard="imageCard"
+    :title="item.title"
+    :description="item.content"
+    :time="item.time"
+    :imageUrl="item.image"
+    shadow="hover"
+    v-for="(item,index) in projectList" :key="index"
+  >
+    <template #image-custom>
+      <el-avatar :src="item.url"></el-avatar>
+    </template>
+  </Card>
+</template>
+<script>
+import {defineComponent, ref} from 'vue';
+import {Card} from '@/components/Card';
+import {projectList} from './data';
+
+export default defineComponent({
+  components: {Card},
+  setup() {
+    const imageCard = ref(true)
+    return {
+      imageCard,
+      projectList
+    }
+  }
+})
+</script>
