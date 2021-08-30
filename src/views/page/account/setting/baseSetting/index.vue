8b442ff48d6fba1c29c04c7018e4e85f6da2522d
wky0527 <805245271@qq.com>
2021-08-30 19:16:17 +0800

    wip: 个人设置-基本设置组件form调整+image上传

--- src/views/page/account/setting/baseSetting/index.vue
+++ src/views/page/account/setting/baseSetting/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="base-setting">
     <h2>基本设置</h2>
-    <el-row :gutter="20">
+    <el-row :gutter="30">
       <el-col :span="16">
-        <el-form :model="form" label-position="top" style="border-right: 1px solid rgb(240, 240, 240);">
+        <el-form :model="form" label-position="top">
           <el-form-item label="邮箱">
             <el-input v-model="form.email" placeholder="请输入邮箱" clearable />
           </el-form-item>
@@ -20,21 +20,33 @@
             </el-select>
           </el-form-item>
           <el-form-item label="所在省市" prop="area">
-            <el-select v-model="form.area" placeholder="请选择地区" style="width:50%">
-              <el-option label="山西省" value="shanxi"></el-option>
-              <el-option label="北京市" value="beijing"></el-option>
-            </el-select>
-            <el-select v-model="form.area" placeholder="请选择地区" style="width: 50%">
-              <el-option label="太原市" value="taiyuan"></el-option>
-              <el-option label="市辖区" value="shixiaqu"></el-option>
-            </el-select>
+            <el-row :gutter="10">
+              <el-col :span="12">
+                <el-select v-model="form.area" placeholder="请选择地区">
+                  <el-option label="山西省" value="shanxi"></el-option>
+                  <el-option label="北京市" value="beijing"></el-option>
+                </el-select>
+              </el-col>
+              <el-col :span="12">
+                <el-select v-model="form.area" placeholder="请选择地区">
+                  <el-option label="太原市" value="taiyuan"></el-option>
+                  <el-option label="市辖区" value="shixiaqu"></el-option>
+                </el-select>
+              </el-col>
+            </el-row>
           </el-form-item>
           <el-form-item label="街道地址" prop="address">
             <el-input v-model="form.address" clearable placeholder="请输入街道地址"/>
           </el-form-item>
           <el-form-item label="联系电活" prop="phone">
-            <el-input v-model="form.phone" clearable size="medium" placeholder="请输入区号"/>
-            <el-input v-model="form.phone" clearable placeholder="请输入联系电活"/>
+            <el-row :gutter="10">
+              <el-col :span="6">
+                <el-input v-model="form.phone" clearable placeholder="请输入区号"/>
+              </el-col>
+              <el-col :span="18">
+                <el-input v-model="form.phone" clearable placeholder="请输入联系电活"/>
+              </el-col>
+            </el-row>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
@@ -42,24 +54,37 @@
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="8" class="base-setting-image">
+        <h3>头像</h3>
+        <el-avatar :src="src" :size="size"></el-avatar>
+        <BasicUpload
+          icon="el-icon-upload"
+          size="medium"
+          type="primary"
 
+        />
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
-import {defineComponent, ref, reactive} from 'vue';
+import {defineComponent, ref, reactive, toRefs} from 'vue';
 import {BasicForm} from "@/components/Form";
-
+import {BasicUpload} from "@/components/Upload";
 export default defineComponent({
-  components: {BasicForm},
+  components: {BasicForm,BasicUpload},
   setup() {
     const labelPosition = ref('top');
     const form = reactive({})
+    const avatar = reactive({
+      src: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+      size: 140
+    })
+    const {src,size} = avatar;
     return {
       labelPosition,
-      form
+      form,
+      ...toRefs(avatar)
     }
   }
 })
@@ -67,5 +92,14 @@ export default defineComponent({
 <style lang="scss" scoped>
 .base-setting {
   margin: -16px 0 0 26px;
+  ::v-deep .el-select--small {
+    width: 100%;
+  }
+  .base-setting-image {
+    @include flex($direction: column);
+    .el-avatar {
+      margin-bottom: 25px;
+    }
+  }
 }
 </style>
