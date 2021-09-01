a65c052685995c39807d2776e82935b7b156cd5d
shiwuhao <shiwuhao@juzishuke.com>
2021-09-01 17:03:44 +0800

    app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-      "no-console": "off",
-      "no-unused-vars": "off",
-      "vue/no-unused-components": "off",
-      "no-unreachable": "off",
-      "no-debugger": "off",
-      "vue/script-setup-uses-vars": "off"
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "vue/no-unused-components": "off",
+    "no-unreachable": "off",
+    "no-debugger": "off",
+    "vue/script-setup-uses-vars": "off",
+    "vue/no-multiple-template-root": "off"
+  }
 };
