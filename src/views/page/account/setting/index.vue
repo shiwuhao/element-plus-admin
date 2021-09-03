8d526e8612f40bd08b25d6599ff260ad184c4c24
wky0527 <805245271@qq.com>
2021-09-03 09:50:56 +0800

    wip: 个人设置引起安全设置组件

--- src/views/page/account/setting/index.vue
+++ src/views/page/account/setting/index.vue
@@ -1,25 +1,29 @@
 <template>
     <el-card class="setting-card">
-        <el-tabs :tab-position="tabPosition" class="setting-tabs">
-          <el-tab-pane label="基本设置">
+        <el-tabs :tab-position="tabPosition" class="setting-tabs" v-model="activeName">
+          <el-tab-pane label="基本设置" name="1">
             <BaseSetting />
           </el-tab-pane>
-          <el-tab-pane label="安全设置">安全设置</el-tab-pane>
-          <el-tab-pane label="账号绑定">账号绑定</el-tab-pane>
-          <el-tab-pane label="新消息通知">新消息通知</el-tab-pane>
+          <el-tab-pane label="安全设置" name="2">
+            <SafeSetting />
+          </el-tab-pane>
+          <el-tab-pane label="账号绑定" name="3">账号绑定</el-tab-pane>
+          <el-tab-pane label="新消息通知" name="4">新消息通知</el-tab-pane>
         </el-tabs>
     </el-card>
 </template>
 <script>
 import {defineComponent,ref} from 'vue';
 import BaseSetting from './baseSetting/index';
+import SafeSetting from './safeSetting/index';
 export default defineComponent({
-   components: {BaseSetting},
+   components: {BaseSetting,SafeSetting},
    setup(){
      const tabPosition = ref('left');
+     const activeName = ref('2');
      return {
        tabPosition,
-
+       activeName
      }
    }
 })
