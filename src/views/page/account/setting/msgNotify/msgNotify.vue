235678e2107adaee8593ecc0b411dc9b85c3fc8c
wky0527 <805245271@qq.com>
2021-09-03 18:02:31 +0800

    wip: 个人设置-新消息页面布局完成

---
+++ src/views/page/account/setting/msgNotify/msgNotify.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="setting-page">
+    <el-descriptions :title="item.title" v-for="item in msgNotifyList" :key="item.key">
+      <el-descriptions-item :label="item.label">{{ item.value }}</el-descriptions-item>
+      <template #extra>
+        <el-switch v-model="item.status"/>
+      </template>
+    </el-descriptions>
+  </div>
+</template>
+<script>
+import {defineComponent, reactive, ref} from 'vue';
+import {msgNotifyList} from '../data';
+export default defineComponent({
+  setup(){
+    // const value = ref(true)
+    return {
+      msgNotifyList,
+      // value
+    }
+  }
+})
+</script>
