4507d197c2497167eb2706abe121d4812e657556
wky0527 <805245271@qq.com>
2021-09-03 18:04:01 +0800

    perf: 优化代码，将if判断渲染组件改为动态加载组件

--- src/views/page/account/setting/index.vue
+++ src/views/page/account/setting/index.vue
@@ -1,45 +1,91 @@
 <template>
-    <el-card class="setting-card">
-        <el-tabs :tab-position="tabPosition" class="setting-tabs" v-model="activeName">
-          <el-tab-pane label="基本设置" name="1">
-            <BaseSetting />
-          </el-tab-pane>
-          <el-tab-pane label="安全设置" name="2">
-            <SafeSetting />
-          </el-tab-pane>
-          <el-tab-pane label="账号绑定" name="3">账号绑定</el-tab-pane>
-          <el-tab-pane label="新消息通知" name="4">新消息通知</el-tab-pane>
-        </el-tabs>
-    </el-card>
+  <el-card class="setting-card">
+    <el-tabs :class="getIsMobile?'mobile-setting-tabs':'setting-tabs'"
+             :tab-position="getIsMobile?'top':'left'"
+             v-model="activeName"
+    >
+      <el-tab-pane
+        v-for="item in settingList"
+        :key="item.key"
+        :label="item.name"
+        :name="item.key"
+      >
+        <component :is="item.component" />
+      </el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 <script>
-import {defineComponent,ref} from 'vue';
-import BaseSetting from './baseSetting/index';
-import SafeSetting from './safeSetting/index';
+import {defineComponent, ref} from 'vue';
+import BaseSetting from './baseSetting/baseSetting';
+import SafeSetting from './safeSetting/safeSetting';
+import AccountBind from './accountBind/accountBind';
+import MsgNotify from './msgNotify/msgNotify';
+import {settingList} from './data';
+import {useRootSetting} from "@/composables/setting/useRootSeeting";
 export default defineComponent({
-   components: {BaseSetting,SafeSetting},
-   setup(){
-     const tabPosition = ref('left');
-     const activeName = ref('2');
-     return {
-       tabPosition,
-       activeName
-     }
-   }
+  components: {BaseSetting, SafeSetting,AccountBind,MsgNotify},
+  setup() {
+    const activeName = ref('4');
+    const {getIsMobile} = useRootSetting();
+    return {
+      activeName,
+      settingList,
+      getIsMobile
+    }
+  }
 })
 </script>
 <style lang="scss" scoped>
 .setting-card {
   margin: 24px;
+
   .setting-tabs {
-    height: 100vh;
-   ::v-deep .el-tabs__nav-wrap  {
+
+    ::v-deep .el-tabs__nav-wrap {
       width: 224px;
-     .el-tabs__nav {
-      .el-tabs__item {
-        text-align: left;
+
+      .el-tabs__nav {
+        .el-tabs__item {
+          text-align: left;
+        }
       }
-     }
+    }
+  }
+  .mobile-setting-tabs {
+    ::v-deep .el-tabs__nav-wrap {
+      width: 100%;
+    }
+  }
+}
+.setting-page {
+  margin-top: 20px;
+  padding-left: 18px;
+  //margin: -16px 0 0 26px;
+  ::v-deep .el-descriptions {
+    margin-bottom: 40px;
+
+    .el-descriptions__header {
+      height: 0;
+
+      .el-descriptions__title {
+        font-size: $font-size-small;
+        color: $color-text-black
+      }
+
+    }
+    .el-descriptions__body {
+      border-bottom: 1px solid $color-border-dashed;
+      padding-bottom: 14px;
+    }
+    .is-left {
+      color: $color-text-gray;
+      font-size: $font-size-small;
+    }
+    .el-descriptions-title {
+      color: $color-text-blue;
+      font-size: $font-size-small;
+      cursor: pointer;
     }
   }
 }
