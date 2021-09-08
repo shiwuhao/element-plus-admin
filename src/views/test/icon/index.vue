286794b3aedfd3a5b60081b1f1c0b590bc97a648
wky0527 <805245271@qq.com>
2021-09-08 18:24:59 +0800

    feat：添加测试页面

---
+++ src/views/test/icon/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-row>
+    <el-col :span="12">
+      <el-card>
+        <IconPicker :iconList="icons"/>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import {defineComponent, ref, onMounted, reactive, computed, unref} from 'vue';
+import iconList from '@/assets/icons/icon';
+import {IconPicker} from '@/components/Icon'
+export default defineComponent({
+  components: {IconPicker},
+  setup() {
+   const icons = reactive(iconList)
+    return {
+      icons
+    }
+  }
+})
+</script>
