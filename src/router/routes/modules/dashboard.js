0a5c70b927b5a66633944319bc5affe727ebc60b
wky0527 <805245271@qq.com>
2021-09-14 19:19:18 +0800

    wip: 添加分析页面路由

--- src/router/routes/modules/dashboard.js
+++ src/router/routes/modules/dashboard.js
@@ -12,7 +12,7 @@ export default [
         path: 'analysis',
         name: 'analysis',
         meta: {title: '分析页', menu: true, cache: true, affix: true},
-        component: () => import('@/views/dashboard/analysis'),
+        component: () => import('@/views/dashboard/analysis/index'),
       },
       {
         path: 'monitor',
