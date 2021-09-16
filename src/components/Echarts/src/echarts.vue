c90fea487b53d7a91a955d0a21779014d7a4504b
wky0527 <805245271@qq.com>
2021-09-16 09:59:55 +0800

    wip: 完善echarts组件，解决多个echarts无法同时渲染的问题，在nextTick中获取完dom再初始化

--- src/components/Echarts/src/echarts.vue
+++ src/components/Echarts/src/echarts.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="echarts-wrap" style="width: 100%;height: 100%">
-    <div id="echarts" ref="echarts" :style="{width: '100%', height: '100%'}"></div>
-  </div>
+    <div :id="id" ref="echarts" :style="{width: '100%', height: '100%'}"></div>
 </template>
 <script>
-import {defineComponent, computed, toRefs, watch, ref, reactive, onMounted, onUnmounted} from 'vue';
+import {defineComponent, computed, toRefs, watch, ref, reactive, onMounted, onUnmounted, nextTick} from 'vue';
 
 let Echarts = require('echarts')
 export default defineComponent({
   //接收父类两个数据 1、chartData (series数据 + x坐标系数据）2、isAxisChart （是否有x坐标系，如果false，那么上面的xData就为空）
   props: {
+    id: {
+      type: String
+    },
     title: {
       type: Object  //标题，可以只传入标题，其他属性使用默认值，也可自定义title属性，默认无标题
     },
@@ -43,9 +44,9 @@ export default defineComponent({
     }
   },
   setup(props, {emit}) {
-    const {chartData, height} = toRefs(props);
-    console.log(chartData.value)
+    const {chartData, height,id,title} = toRefs(props);
     const echarts = ref(null);
+    const echartsId= ref(null);
     const dynamicData = reactive({});
     const baseTitle = reactive({
       text: '', // 主标题
@@ -57,6 +58,7 @@ export default defineComponent({
         fontSize: 18
       }
     })
+    // 默认dom属性
     const baseDom = reactive({
       theme: 'light',
       renderer: 'canvas',
@@ -65,32 +67,31 @@ export default defineComponent({
         height: 'auto'
       }
     })
-    const options = computed(() => {
-      dynamicData.value = chartData;
-      return dynamicData.value
-    })
+
+    const initChart = () => {
+      nextTick(()=>{
+        echartsId.value = document.getElementById(id.value);
+        const myChart = Echarts.init(echartsId.value,baseDom.opts.theme,{
+          width: baseDom.opts.width,
+          height: baseDom.opts.height
+        })
+        myChart.setOption(chartData.value)
+      })
+    }
+    //watch里调用方法，需要把watch写在方法的下面
     watch(
-      ([height]), ([val, oldVal]) => {
-        baseDom.opts.height = val;
+      ([height,chartData]), ([val, oldVal]) => {
+
       },
       {
         immediate: true,
         deep: true
       }
     );
-    const initChart = () => {
-      //获取echarts对象
-      const myChart = Echarts.init(document.getElementById('echarts'), baseDom.theme, {
-        width: baseDom.opts.width,
-        height: baseDom.opts.height
-      });
-       myChart.setOption(options.value)
-    }
     const resizeChart = () => {
-      echarts.value ? echarts.value.resize() : ''
+       echarts.value.resize()
     }
     onMounted(() => {
-      initChart()
       //resize 改变图表尺寸，在容器大小发生改变时需要手动调用（因为侧边栏是可以收缩的，所以这里图表根据是否收缩来改变图表尺寸）
       window.addEventListener('resize', resizeChart)
     })
@@ -100,10 +101,10 @@ export default defineComponent({
     return {
       initChart,
       resizeChart,
-      options,
       baseTitle,
-      baseDom,
-      dynamicData
+      dynamicData,
+      echartsId,
+      baseDom
     }
   }
 })
