88c248f7385c5797a84b82c118666eeb12fa162d
wky0527 <805245271@qq.com>
2021-09-22 19:46:52 +0800

    fix: 解决echarts组件在屏幕大小发生变化式，resize事件不生效

--- src/components/Echarts/src/echarts.vue
+++ src/components/Echarts/src/echarts.vue
@@ -40,9 +40,18 @@ export default defineComponent({
     const {chartData, height, id, title} = toRefs(props);
     const echartsId = ref(null);
     const myChart = ref(null);
+    const propTitle = ref('');
+    switch (typeof title.value) {
+      case 'string':
+        propTitle.value = title.value;
+        break;
+      case 'object':
+        propTitle.value  = title.value && title.value.text;
+        break;
+    }
     const option = reactive({
       title: {
-        text: title.value && title.value.text || '',
+        text:  propTitle.value,
       },
       tooltip: {
         // trigger: 'axis'
@@ -86,23 +95,27 @@ export default defineComponent({
         deep: true
       }
     );
-    const resizeChart = () => {
-      myChart.value && myChart.value.resize()
+    // const resizeChart = () => {
+    //   myChart.value && myChart.value.resize()
+    // }
+    // onMounted(() => {
+    //   //resize 改变图表尺寸，在容器大小发生改变时需要手动调用（因为侧边栏是可以收缩的，所以这里图表根据是否收缩来改变图表尺寸）
+    // //  window.addEventListener('resize', resizeChart)
+    // })
+    // onUnmounted(() => {
+    //   window.removeEventListener('resize', resizeChart)
+    // })
+    window.onresize = ()=> {
+      myChart.value.resize()
     }
-    onMounted(() => {
-      //resize 改变图表尺寸，在容器大小发生改变时需要手动调用（因为侧边栏是可以收缩的，所以这里图表根据是否收缩来改变图表尺寸）
-      window.addEventListener('resize', resizeChart)
-    })
-    onUnmounted(() => {
-      window.removeEventListener('resize', resizeChart)
-    })
     return {
       initChart,
-      resizeChart,
+      //resizeChart,
       echartsId,
       baseDom,
       option,
-      myChart
+      myChart,
+      propTitle
     }
   }
 })
