b6399e291a1f8bb0d2ffc384e0bd7bc34107e9b1
wky0527 <805245271@qq.com>
2021-09-29 16:05:47 +0800

    feat: 监控页面排版

--- src/views/dashboard/monitor/index.vue
+++ src/views/dashboard/monitor/index.vue
@@ -1,3 +1,37 @@
 <template>
-  监控
+    <div class="m10">
+      <el-row :gutter="20">
+        <el-col :span="16">
+          <LeftContent/>
+        </el-col>
+        <el-col :span="8">
+          <RightContent/>
+        </el-col>
+      </el-row>
+    </div>
 </template>
+<script>
+import {defineComponent} from 'vue';
+import LeftContent from "./components/LeftContent/LeftContent";
+import RightContent from "./components/RightContent/RightContent";
+export default defineComponent({
+  components: {LeftContent,RightContent},
+  setup(){
+    return {
+
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+::v-deep .el-card__header {
+  padding: 16px 20px !important;
+
+  .card-header {
+    span {
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
+}
+</style>
