f511ad3f64f05b2589662e40f64d96d49ab94677
wky0527 <805245271@qq.com>
2021-09-29 16:06:44 +0800

    feat：工作台页面做移动端兼容

--- src/views/dashboard/workplace/index.vue
+++ src/views/dashboard/workplace/index.vue
@@ -1,25 +1,42 @@
 <template>
   <HeaderContent/>
-  <el-row>
-    <el-col :span="16">
-      <LeftContent/>
-    </el-col>
-    <el-col :span="8">
-      <RightContent/>
-    </el-col>
-  </el-row>
+  <div class="m10">
+    <el-row :gutter="20">
+      <el-col :span="getIsMobile?24:16">
+        <LeftContent/>
+      </el-col>
+      <el-col :span="getIsMobile?24:8">
+        <RightContent/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
 import {defineComponent} from 'vue';
 import HeaderContent from './components/HeaderContent/HeaderContent';
 import LeftContent from './components/LeftContent/LeftContent';
 import RightContent from './components/RightContent/RightContent';
+import {useRootSetting} from "@/composables/setting/useRootSeeting";
 
 export default defineComponent({
   components: {HeaderContent, LeftContent, RightContent},
   setup() {
-
-    return {}
+    const {getIsMobile} = useRootSetting()
+    return {
+      getIsMobile
+    }
   }
 })
 </script>
+<style lang="scss" scoped>
+::v-deep .el-card__header {
+  padding: 16px 20px !important;
+
+  .card-header {
+    span {
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
+}
+</style>
