1781f4d4f12805245b769b9c893ca464f58e2162
shiwuhao <shiwuhao@juzishuke.com>
2021-10-09 14:34:43 +0800

    防抖 节流

--- src/router/routes/modules/feature.js
+++ src/router/routes/modules/feature.js
@@ -9,23 +9,53 @@ export default [
     meta: {title: '功能', icon: 'el-icon-set-up', menu: true},
     children: [
       {
-        path: 'full-screen',
-        name: 'full-screen',
-        meta: {title: '全屏', menu: true, affix: false},
-        component: () => import('@/views/feature/full-screen/index.vue'),
-      },
-      {
         path: 'context-menu',
         name: 'context-menu',
         meta: {title: '右键菜单', menu: true, affix: false},
         component: () => import('@/views/feature/context-menu/index.vue'),
       },
       {
+        path: 'full-screen',
+        name: 'full-screen',
+        meta: {title: '全屏', menu: true, affix: false},
+        component: () => import('@/views/feature/full-screen/index.vue'),
+      },
+      {
         path: 'clipboard',
         name: 'clipboard',
         meta: {title: '剪贴板', menu: true, affix: false},
         component: () => import('@/views/feature/clipboard/index.vue'),
-      }
+      },
+      {
+        path: 'click-out-side',
+        name: 'click-out-side',
+        meta: {title: '点击外部', menu: true, affix: false},
+        component: () => import('@/views/feature/click-out-side/index.vue'),
+      },
+      {
+        path: 'qr-code',
+        name: 'qr-code',
+        meta: {title: '二维码', menu: true, affix: false},
+        component: () => import('@/views/feature/qr-code/index.vue'),
+      },
+      {
+        path: 'drauu',
+        name: 'drauu',
+        meta: {title: '画图', menu: true, affix: false},
+        component: () => import('@/views/feature/drauu/index.vue'),
+      },
+      {
+        path: 'debounce',
+        name: 'debounce',
+        meta: {title: '防抖', menu: true, affix: false},
+        component: () => import('@/views/feature/debounce/index.vue'),
+      },
+      {
+        path: 'throttle',
+        name: 'throttle',
+        meta: {title: '节流', menu: true, affix: false},
+        component: () => import('@/views/feature/throttle/index.vue'),
+      },
     ]
   }
 ];
---
+++ src/views/feature/drauu/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <page-wrapper :title="$route.meta['title']">
+    <template #content>
+      使用VueUse useDrauu
+      <el-link type="primary" href="https://vueuse.org/integrations/useDrauu/" target="_blank">查看</el-link>
+    </template>
+      <svg ref="target" style="width:500px;height: 500px;background: white;"></svg>
+  </page-wrapper>
+</template>
+<script>
+import {PageWrapper} from '@/components/page'
+import {useDrauu} from '@vueuse/integrations/useDrauu'
+import {ref,toRefs} from "vue";
+
+export default {
+  name: "index",
+  components: {PageWrapper},
+  setup() {
+    const target = ref()
+    const { undo, redo, canUndo, canRedo, brush } = useDrauu(target)
+    const { color, size } = toRefs(brush)
+
+    return {
+      target,
+      undo, redo, canUndo, canRedo, brush,color, size
+    }
+  }
+}
+</script>
