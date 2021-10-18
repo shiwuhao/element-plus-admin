6e1ab43cbb3ae97878d6c2a47abddaa028b90c80
shiwuhao <shiwuhao@juzishuke.com>
2021-10-18 14:43:52 +0800

    Updates

--- assets/index.b44540be.js
+++
@@ -1,1 +0,0 @@
-import{y as a,g as c,o as n,c as p}from"./vendor.ecb85c6e.js";import{_ as u}from"./index.09ade0ae.js";const d={setup(){const{currentRoute:e,replace:r}=a(),{params:t,query:o}=c(e),{path:s}=t;r({path:"/"+s,query:o})}};function _(e,r,t,o,s,f){return n(),p("div")}var l=u(d,[["render",_]]);export{l as default};
--- assets/useUser.412674b6.js
+++
@@ -1,1 +0,0 @@
-import{x as r,f as s}from"./vendor.ecb85c6e.js";function n(){const{getters:e}=r(),t=s(()=>e.getUser),o=s(()=>e.getAccessToken);return{getUser:t,getToken:o}}export{n as u};
