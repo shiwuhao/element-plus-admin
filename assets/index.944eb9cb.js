09edfc58adec9db937d4e066272394ee74bfa989
shiwuhao <shiwuhao@juzishuke.com>
2021-10-18 10:17:12 +0800

    Updates

--- assets/index.944eb9cb.js
+++
@@ -1,1 +0,0 @@
-import{y as a,g as n,o as c,c as p}from"./vendor.0028b10d.js";import{_ as u}from"./index.50770654.js";const d={setup(){const{currentRoute:e,replace:r}=a(),{params:t,query:o}=n(e),{path:s}=t;r({path:"/"+s,query:o})}};function _(e,r,t,o,s,f){return c(),p("div")}var l=u(d,[["render",_]]);export{l as default};
