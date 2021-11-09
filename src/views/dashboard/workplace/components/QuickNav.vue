70a80d9b181e00ae1aa22b127c80b7085859c111
shiwuhao <shiwuhao@juzishuke.com>
2021-11-09 15:18:44 +0800

    error page

--- src/views/dashboard/workplace/components/QuickNav.vue
+++ src/views/dashboard/workplace/components/QuickNav.vue
@@ -3,8 +3,8 @@
     <el-row>
       <el-col :span="8" v-for="(item,index) in getMenus" :key="index" class="item">
         <el-card class="card" shadow="hover">
-          <div class="flex-col align-center">
-            <span class="text-xl"><i :class="item.icon" :style="{color:item.color}"></i></span>
+          <div class="flex-col align-center" style="cursor: pointer">
+            <span class="text-xl"><icon :name="item.icon" :style="{color:item.color}" :size="20" /></span>
             <span>{{ item.title }}</span>
           </div>
         </el-card>
--- src/views/demo/permission/page.vue
+++ src/views/demo/permission/page.vue
@@ -1,13 +1,15 @@
 <template>
   <page-wrapper :title="$route.meta['title']">
     <template #content>
-      <div class="text-secondary">
-        基于后台的权限模式，后台返回用户的权限节点，包含菜单，角色，动作<br>
-        mock了两组数据， id为1 和 2 具体返回的数据可以在mock/personal.js内查看<br>
-        菜单：当前用户拥有的菜单权限<br>
-        角色：当前用户拥有的角色权限<br>
-        动作：当前用户拥有的角色所包含的的动作节点合集<br><br>
-        <el-alert title="用户切换后查看左侧菜单变化" type="success"/>
+      <div class="text-secondary text-sm">
+        <el-alert title="" type="info">
+          基于后台的权限模式，后台返回用户的权限节点，包含菜单，角色，动作<br>
+          mock了两组数据， id为1 和 2 具体返回的数据可以在mock/personal.js内查看<br>
+          菜单：当前用户拥有的菜单权限<br>
+          角色：当前用户拥有的角色权限<br>
+          动作：当前用户拥有的角色所包含的的动作节点合集<br><br>
+          用户切换后查看左侧菜单变化
+        </el-alert>
       </div>
     </template>
 
@@ -19,17 +21,12 @@
 <script>
 import UserPermissions from "@/views/demo/permission/UserPermissions.vue";
 import {PageWrapper} from "@/components/Page"
-import {defineComponent, toRefs, reactive} from "vue";
+import {defineComponent} from "vue";
 
 
 export default defineComponent({
   name: "index",
   components: {PageWrapper, UserPermissions},
-  setup() {
-
-
-    return {}
-  },
 })
 </script>
 
